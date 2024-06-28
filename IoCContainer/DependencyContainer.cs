067d0da285aba5fe2378c1230ff6e18b15745420
Andrija Eremic <andrija.eremic@gmail.com>
2024-06-28 22:07:11 +0200

    test

---
+++ IoCContainer/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace IoCContainer;
+
+public class DependencyContainer
+{
+    private List<Type> _dependencies = new();
+
+    public void AddDependency(Type type)
+    {
+        _dependencies.Add(type);
+    }
+    
+    public void AddDependency<T>()
+    {
+        _dependencies.Add(typeof(T));
+    }
+
+    public Type GetDependency(Type type)
+    {
+        return _dependencies.First(d => d.Name == type.Name);
+    }
+}
---
+++ IoCContainer/DependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿namespace IoCContainer;
+
+public class DependencyResolver
+{
+    private DependencyContainer _container;
+
+    public DependencyResolver(DependencyContainer container)
+    {
+        _container = container;
+    }
+
+    public T GetService<T>()
+    {
+        return (T)GetService(typeof(T));
+    }
+
+    public object GetService(Type type)
+    {
+        var dependency = _container.GetDependency(type);
+        var constructor = dependency.GetConstructors().Single();
+        var parameters = constructor.GetParameters().ToArray();
+
+        if (parameters.Length > 0)
+        {
+            var parametersImplementations = new object[parameters.Length];
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                parametersImplementations[i] = GetService(parameters[i].ParameterType);
+            }
+
+            return Activator.CreateInstance(dependency, parametersImplementations) ??
+                   throw new InvalidOperationException();
+        }
+
+        return Activator.CreateInstance(dependency) ?? throw new InvalidOperationException();
+    }
+}
